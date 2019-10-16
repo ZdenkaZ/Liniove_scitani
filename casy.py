1c43d361fc054797dfffa449f1af088c26d7961d
ZdenkaZ <zdenkaherova@gmail.com>
2019-10-16 19:50:45 +0200

    pyth

---
+++ casy.py
@@ -0,0 +1,20 @@
+#casy = [12, 25, 64, 27, 15, 66, 128, 44]
+#import sys
+# casy = [int(h) for h in sys.argv[1:]]
+# do_hodiny = [h for h in casy if h // 60 < 1]
+# print(do_hodiny)
+# pres_hodinu = [(h-60) for h in casy if h > 60]
+# print(pres_hodinu)
+
+# celsius = [int(f) for f in sys.argv[1:]]
+# stupne = [((f-32)*5/9) for f in celsius]
+# print(stupne)
+
+# #split, join
+# cesta = 'cesta\k\souboru'
+# print('I\'m happy.')
+# print('I\'m\nhappy.')
+# #print('I'm happy.)
+# print('I\'m happy\neverything is tremendous\t/\tcharming.')
+
+
